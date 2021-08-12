dd2b96a80e3466d07d6c7fdb3e9cd12ac3b6389a
ShoukkiyaAshraf <shoukkiya59@gmail.com>
2021-08-12 21:51:33 +0530

    About Error - 12/08/2021

--- REACTJS/react-complete-guide/src/App.js
+++ REACTJS/react-complete-guide/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 class App extends Component {
   render() {
     return (
