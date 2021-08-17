797b8e63d1a67738c5de0f0d46980df34f8e3413
Shoukkiya Ashraf <shoukkiya.ashraf@speridian.com>
2021-08-17 20:59:17 +0530

    17/08/2021 change #1

--- REACTJS/my-app/src/App.js
+++ REACTJS/my-app/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
+import person from './Person/Person';
 
 function App() {
   return (
@@ -7,8 +9,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Aloo <code>React :)</code> ..
         </p>
+        <Person></Person>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ REACTJS/my-app/src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = () => {
+    return <p>I'm a Person! AND I'm {Math.floor(Math.random()*10)} years old ..</p>
+};
+
+export default person;
