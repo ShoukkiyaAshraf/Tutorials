d650359e37f8f5be2bb575b500920a396cba1956
Shoukkiya Ashraf <shoukkiya.ashraf@speridian.com>
2021-08-17 18:24:21 +0530

    17/08/2021 Added my-app

--- REACTJS/react-complete-guide/src/App.js
+++ REACTJS/react-complete-guide/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App :) </h1>
+        <p> This is really working!</p>
       </div>
     );
   }
---
+++ REACTJS/react-complete-guide/src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = () => {
+    return <p>I'am a Person!</p>
+};
+
+export default person;
