6c1761a0b0e5fa9d1459001f49e123ef50101073
Shoukkiya Ashraf <shoukkiya59@gmail.com>
2021-09-02 11:20:02 +0530

    Controlling - useEffect hook for functional Component

--- REACTJS/my-app/src/components/Cockpit/Cockpit.js
+++ REACTJS/my-app/src/components/Cockpit/Cockpit.js
@@ -6,7 +6,13 @@ const Cockpit = ( props ) => {
 
     useEffect(() => {
         console.log('[Cockpit.js] useEffect');
-    });
+        // Http request..
+        setTimeout(() => {
+            alert('Saved data to cloud!');
+        }, 1000);
+    }, []);
+
+    //useEffect
 
     const assignedClasses = [];
     let btnClass = '';
