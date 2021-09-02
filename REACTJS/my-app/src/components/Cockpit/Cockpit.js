e8a329e89177d0ccf4d66facee6ac0322084cbd8
Shoukkiya Ashraf <shoukkiya59@gmail.com>
2021-09-02 12:46:22 +0530

    cleanup example

--- REACTJS/my-app/src/components/Cockpit/Cockpit.js
+++ REACTJS/my-app/src/components/Cockpit/Cockpit.js
@@ -7,10 +7,11 @@ const Cockpit = ( props ) => {
     useEffect(() => {
         console.log('[Cockpit.js] useEffect');
         // Http request..
-        setTimeout(() => {
+        const timer = setTimeout(() => {
             alert('Saved data to cloud!');
         }, 1000);
         return () => {
+            clearTimeout(timer);
             console.log('[Cockpit.js] cleanup in useEffect');
         };
     }, []);
