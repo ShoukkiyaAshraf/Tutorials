638f12be3f126e94f4cd92eb8ed028cafa3ec28d
Shoukkiya Ashraf <shoukkiya.ashraf@speridian.com>
2021-09-02 10:21:29 +0530

    component life cycle

--- REACTJS/my-app/src/components/Persons/Person/Person.js
+++ REACTJS/my-app/src/components/Persons/Person/Person.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from './Person.css';
 
-const person = props => {
-  console.log('[Person.js] rendering...');
-  return (
-    <div className='Person'>
-      <p onClick={props.click}>
-        I'm {props.name} and I am {props.age} years old!
-      </p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
-  );
+class Person extends Component{
+    render(){
+        console.log('[Person.js] rendering...');
+        return (
+            <div className='Person'>
+              <p onClick={this.props.click}>
+                I'm {this.props.name} and I am {this.props.age} years old!
+              </p>
+              <p>{this.props.children}</p>
+              <input type="text" onChange={this.props.changed} value={this.props.name} />
+            </div>
+          );
+    }
 };
 
-export default person;
+export default Person;
