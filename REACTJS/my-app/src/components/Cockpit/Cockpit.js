9ab67fcedb9e4db9b7be408625841da73400c6d1
Shoukkiya Ashraf <shoukkiya59@gmail.com>
2021-09-02 11:13:25 +0530

    useEffect hook for functional Component

--- REACTJS/my-app/src/components/Cockpit/Cockpit.js
+++ REACTJS/my-app/src/components/Cockpit/Cockpit.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import classes from './Cockpit.css';
 
-const cockpit = ( props ) => {
+const Cockpit = ( props ) => {
+
+    useEffect(() => {
+        console.log('[Cockpit.js] useEffect');
+    });
+
     const assignedClasses = [];
     let btnClass = '';
     if (props.showPersons) {
@@ -28,4 +33,4 @@ const cockpit = ( props ) => {
     );
 };
 
-export default cockpit;
+export default Cockpit;
